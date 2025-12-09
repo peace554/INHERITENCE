f6e902066b6a856cf895304f84085f55b3915331
peace554 <phionapeace53@gmail.com>
2025-12-09 19:42:10 +0300

    commit2
    I outputted the two instance variables using displayInfor method

---
+++ workings/src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    String name;
+    int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+        public void displayInfor(){
+            System.out.println("Animal Name" + name);
+            System.out.println("Age" + age);
+        }
+    }
+}
