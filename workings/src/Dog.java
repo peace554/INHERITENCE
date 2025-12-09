ddf9039857892f647670b33434ec7f1b81229c2d
peace554 <phionapeace53@gmail.com>
2025-12-09 19:58:16 +0300

    commit2
    from line 12 this class overrides the displayinfor method from the class Animal it adds extra details specific to the dog class(breed)

---
+++ workings/src/Dog.java
@@ -0,0 +1,17 @@
+public class Dog extends Animal {
+    String name;
+    int age;
+    String breed;
+
+    public Dog(String name, int age, String name1, String breed, int age1) {
+        super(name, age);
+        this.name = name1;
+        this.breed = breed;
+        this.age = age1;
+    }
+    //@Override
+    public void displaystatus(){
+        super.displayInfor();
+        System.out.println("Breed:"+breed);
+    }
+}
