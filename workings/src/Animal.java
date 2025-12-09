b28297105d3d27cdbaea8416267050c39ceae7c4
peace554 <phionapeace53@gmail.com>
2025-12-09 19:48:44 +0300

    commit1
    i created a class name Dog and declared three instance variables the from line 6 to 10 in new version i generated a constructor for the instance variables

--- workings/src/Animal.java
+++ workings/src/Animal.java
@@ -5,9 +5,10 @@ public class Animal {
     public Animal(String name, int age) {
         this.name = name;
         this.age = age;
-        public void displayInfor(){
+    }
+        public void displayInfor () {
             System.out.println("Animal Name" + name);
             System.out.println("Age" + age);
+
         }
     }
-}
