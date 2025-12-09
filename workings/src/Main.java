965f02b94f031426b9d66e3d71aa6182cd3828a5
peace554 <phionapeace53@gmail.com>
2025-12-09 20:17:42 +0300

    commit1
    i called the overridden displayinfor method to show the  specified breed information

--- workings/src/Main.java
+++ workings/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         Dog DOG = new Dog("daisy",12,"lynette","German sherpard",23);
         Animal A =new Animal("Gloria",54);
-
+DOG.displayInfor();
 
     }
 
