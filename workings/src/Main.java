f68872ec77ef41b54983b34fc7b3653eea33ac46
peace554 <phionapeace53@gmail.com>
2025-12-09 20:13:52 +0300

    commit2
    The new version line 4 i also created the object Animal class and called its method

--- workings/src/Main.java
+++ workings/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
         Dog DOG = new Dog("daisy",12,"lynette","German sherpard",23);
+        Animal A =new Animal("Gloria",54);
 
 
     }
